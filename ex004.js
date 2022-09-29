f0c2fc92cc5efb3ebf05d116116876c99dee34f2
Larissa Rosa <larissa_rosa7@hotmail.com>
2022-09-29 15:18:09 -0300

    document write, length, upper case, lower case

---
+++ ex004.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é o seu nome? ')
+document.write(`Olá, <strong>${nome}</strong>. Seu nome tem ${nome.length} letras.`)
+document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()}.`)
+document.write(`Seu nome em minúsculas é ${nome.toLowerCase()}.`)
