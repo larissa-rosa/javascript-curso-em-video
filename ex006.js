5bfffd9c9f2413e1deb7365367ad9851df2b2a03
Larissa Rosa <larissa_rosa7@hotmail.com>
2022-12-21 15:09:09 -0300

    addEventListener, function, innerText, getElementByID

---
+++ ex006.js
@@ -0,0 +1,16 @@
+var a = window.document.getElementById('area')
+a.addEventListener('click', clicar)
+a.addEventListener('mouseenter', entrar)
+a.addEventListener('mouseout', sair)
+
+function clicar() {
+  a.innerText = 'Clicou!'
+  a.style.background = 'red'
+}
+function entrar() {
+  a.innerText = 'Entrou!'
+}
+function sair() {
+  a.innerText = 'Saiu!'
+  a.style.background = 'green'
+}
